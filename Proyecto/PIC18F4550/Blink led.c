37563af944946145199641f82d83e855b96c1a8e
vhperaltaj <victor.peralta@microside.com>
2023-08-30 13:03:21 -0600

    Updated identantion

--- Proyecto/PIC18F4550/Blink led.c
+++ Proyecto/PIC18F4550/Blink led.c
@@ -15,23 +15,19 @@ en la tarjeta X-TRAINER
 ---------------------------------------------------------------------------
 */
 
-#include <18F4550.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=48Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
-#build(reset=0x02000,interrupt=0x02008)         //Asignación de los vectores de reset e interrupción
-#org 0x0000,0x1FFF {}                           //Reserva espacio en la memoria para la versión con bootloader
-#define LED PIN_A4                              //Pin donde está conectado el LED del X-TRAINER
+#include <18F4550.h>                           // Incluye el microcontrolador con el que se va a trabajar
+#use delay( clock = 48Mhz, crystal )           // Tipo de oscilador y frecuencia dependiendo del microcontrolador
+#build( reset = 0x02000, interrupt = 0x02008 ) // Asignación de los vectores de reset e interrupción
+#org 0x0000, 0x1FFF {}                         // Reserva espacio en la memoria para la versión con bootloader
+#define LED PIN_A4                             // Pin donde está conectado el LED del X-TRAINER
 
-void main(void)
+void main( void ) {
+   set_tris_a( 0xF7 );                         // Pin RA4 como salida
 
-  {
-     set_tris_a(0xF7);                         //Pin RA4 como salida
-     
-     while(1)
-
-           {
-                  output_HIGH (LED);            //Cambio de estado en el pin RA4
-                  delay_ms (500);               //Retardo
-                  output_LOW (LED);             //Cambio de estado en el pin RA4     
-                  delay_ms (500) ;              //Retardo
-           }
+   while ( 1 ) {
+      output_HIGH( LED );                      // Cambio de estado en el pin RA4
+      delay_ms( 500 );                         // Retardo
+      output_LOW( LED );                       // Cambio de estado en el pin RA4
+      delay_ms( 500 );                         // Retardo
+   }
 }
